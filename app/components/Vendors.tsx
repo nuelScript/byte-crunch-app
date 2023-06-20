5ef0c5eb8d9678bf117a775f22f30948ad197afe
Ibiang Emmanuel <igwee3333@gmail.com>
2023-06-20 14:58:58 +0100

    Fixes

--- app/components/Vendors.tsx
+++ app/components/Vendors.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import Link from 'next/link';
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import Link from "next/link";
 
 interface VendorProps {
   vendor: {
@@ -12,24 +13,25 @@ interface VendorProps {
   };
 }
 
-const Vendors: React.FC<VendorProps> = ({vendor}) => {
+const Vendors: React.FC<VendorProps> = ({ vendor }) => {
   return (
     <div>
       <Link href={`/vendor/${vendor.slug}`}>
-        <div className="relative h-[350px] w-[350px] scale-100 cursor-pointer rounded-2xl transition hover:scale-110 mb-16">
+        <div className="relative mb-16 h-[350px] w-[350px] scale-100 cursor-pointer rounded-2xl transition hover:scale-110">
           <img
             src={vendor.image}
             width={350}
             height={250}
             className="h-[350px] scale-100 rounded-2xl bg-[#ebebeb] object-cover"
+            alt={vendor.name}
           />
-           <div className='absolute bottom-0 left-0 w-full h-[100px] bg-white opacity-70 rounded-b-2xl flex items-center px-3'>
-                <h2 className='text-lg font-bold'>{vendor.name}</h2>
-           </div>
+          <div className="absolute bottom-0 left-0 flex h-[100px] w-full items-center rounded-b-2xl bg-white px-3 opacity-70">
+            <h2 className="text-lg font-bold">{vendor.name}</h2>
+          </div>
         </div>
       </Link>
     </div>
   );
-}
+};
 
 export default Vendors;
